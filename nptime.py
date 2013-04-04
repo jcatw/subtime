f6b5dbf8d55475a4607554b71c5a0971dc71eaa7
James Atwood <jatwood@cs.umass.edu>
2013-04-03 22:17:53 -0400

    initial

---
+++ nptime.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+"""Non-Pedantic Time
+=================
+
+.. moduleauthor:: Thomas Grenfell Smith <thomathom@gmail.com>
+
+Python's :mod:`datetime` module has many uses, but it has a difficulty: you
+can't do any arithmetic with :class:`datetime.time`.  Only with
+:class:`datetime.datetime`.  Now, there are good reasons for this:  "What time
+will it be, 24 hours from now" has a lot of corner cases, including daylight
+savings time, leap seconds, historical timezone changes, and so on.  But
+sometimes you really *do* need the simple case.  Sometimes the perfect is the
+enemy of the good.  And that's why you have this module.  This module will allow
+you to cocoon yourself in the comforting illusion that, in 24 hours, it will be
+the same time as it is right now.
+
+And what freedom this illusion gives you!  You can add a
+:class:`datetime.timedelta` object to an :mod:`nptime` object, and it works! It
+will cycle around 24 hours, like modular arithmetic.  You can ask "what time
+comes 1 day and 36 minutes after 12:24 pm?" and it will let you know: 1:00 pm.
+How lovely!
+
+>>> from nptime import nptime
+>>> from datetime import timedelta, date, datetime
+>>> afternoon = nptime(12, 24) + timedelta(days=1, minutes=36)
+>>> afternoon
+nptime(13, 0)
+>>> str(afternoon)
+'13:00:00'
+
+Maybe we're talking about the afternoon of Guido van Rossum's birthday.  Lucky,
+we can combine ``nptime`` objects with ``date`` objects:
+
+>>> datetime.combine(date(1956, 1, 31), afternoon)
+datetime.datetime(1956, 1, 31, 13, 0, 0)
+
+You can also ask "How long is it between 9:00 AM and 5:00 PM?  It sure
+feels like a million years!"
+
+>>> workday = nptime(hour=17) - nptime(hour=9)
+>>> workday
+datetime.timedelta(0, 28800)
+>>> print workday
+8:00:00
+
+Nope, only 8 hours, how strange.  Anyway, please use this module.  It will
+be convenient.  But don't use it when talking about concrete time, time in
+a particular place, or anything like that.  In fact, it doesn't even notice
+:class:`datetime.tzinfo` objects right now.  Good luck.
+
+.. note::
+    You can find the newest version at https://github.com/tgs/nptime .  You can
+    find online documentation at http://tgs.github.com/nptime/ .  The package
+    is available for download from PyPi:  `easy_install nptime`
+
+"""
+
+from datetime import *
+
+
+class nptime(time):
+    """nptime - a non-pedantic time object
+
+    Inherits from :class:`datetime.time`.  You can't do arithmetic with that
+    class, but with ``nptime`` you can:
+
+    * Add a :class:`datetime.timedelta` to an :class:`nptime` (commutative),
+        resulting in another ``nptime``
+    * Subtract a ``timedelta`` from an ``nptime``, resulting in a 
+        ``timedelta``
+    * Subtract an ``nptime`` from another ``nptime``, resulting in a 
+        ``timedelta``
+
+    You can use the class methods :func:`from_timedelta` and :func:`from_time`
+    to construct an ``nptime`` from those ``datetime`` classes.  You can use the
+    :func:`to_timedelta` instance method to construct a ``timedelta`` from an
+    ``nptime``.
+
+    I can't figure out a reason you'd want to add two times together without
+    using ``timedelta`` objects, but if someone requests that, I'm happy to
+    include it.
+
+    ``nptime`` is `naive` about time zones.  I think this is the only way that
+    makes sense, since days are only 24 hours long in an abstract sense.  But
+    if you want to see a different system, pull requests are welcome..."""
+
+    _std_date = date(year=1000, month=1, day=1)
+
+    @classmethod
+    def from_time(cls, other):
+        """Construct an :class:`nptime` object from a :class:`datetime.time`
+
+        .. note::
+            This *ignores* the :class:`datetime.tzinfo` that may be part
+            of the ``time`` object.
+        """
+        return cls(other.hour, other.minute, other.second, other.microsecond)
+
+    @classmethod
+    def from_timedelta(cls, other):
+        """Construct an :class:`nptime` object from a :class:`datetime.timedelta`.
+
+        The ``timedelta`` is taken to be the amount of time since midnight."""
+        return cls() + other
+
+    def to_timedelta(self):
+        """Construct a :class:`timedelta` object from an :class:`nptime`.  The
+        timedelta gives the number of seconds (and microseconds) since
+        midnight."""
+        return timedelta(hours=self.hour, minutes=self.minute,
+                seconds=self.second, microseconds=self.microsecond)
+
+    def __add__(self, other):
+        """Add nptime and timedelta, returning an nptime"""
+        self_dt = datetime.combine(self._std_date, self)
+        sum = self_dt + other
+        return nptime.from_time(sum.time())
+
+    def __radd__(self, other):
+        """Add timedelta and nptime"""
+        return self + other
+
+    def __sub__(self, other):
+        """Subtract timedelta or nptime from nptime, returning a timedelta"""
+        if isinstance(other, self.__class__):
+            diff = self.to_timedelta() - other.to_timedelta()
+        else:
+            diff = self.to_timedelta() - other
+
+        return diff
+
+
+
+
---
+++ subtime.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# Add (or subtract) an offset to a .srt-formatted subtitle file
+#  arg1: input file name
+#  arg2: offset of form hh:mm:ss,mss or -hh:mm:ss,mss
+#  arg3: output file
+#
+# Author: James Atwood
+# Email: jatwood@cs.umass.edu
+# Written: 4/3/13
+# Why: Spanish horror overdubs just can't quite measure up
+# 
+# nptime was written by Thomas Grenfell Smith <thomathom@gmail.com>
+#
+# Released under the MIT license; see LICENSE for details.
+# 
+from datetime import time, timedelta
+import nptime as npt
+import re
+import sys
+
+def main(input_file, offset_string, output_file):
+    subtract = False
+    
+    if offset_string[0] == '-':
+        subtract = True
+        offset_string = offset_string[1:]
+
+    offset_match = re.match("([0-9]{2}):([0-9]{2}):([0-9]{2}),([0-9]{3})", offset_string)
+    
+    
+    if offset_match is None:
+        print "Malformed offset: %s" % offset_string
+        print "Expected forms: hh:mm:ss,mss or -hh:mm:ss,mss"
+        sys.exit()
+        
+    offset_groups = offset_match.groups()
+    offset_time = timedelta(hours=int(offset_groups[0]),
+                            minutes=int(offset_groups[1]),
+                            seconds=int(offset_groups[2]),
+                            microseconds=1000 * int(offset_groups[3]))  #goes straight to microseconds from seconds(?!)
+
+    subout = open(output_file, 'w')
+    for line in open(input_file, 'r'):
+        line = line.strip()
+
+        #matches a timestamp and extracts
+        #h1, m1, s1, ms1, h2, m2, s2, ms2
+        timestamp_match = re.match("([0-9]{2}):([0-9]{2}):([0-9]{2}),([0-9]{3}) --> ([0-9]{2}):([0-9]{2}):([0-9]{2}),([0-9]{3})",line)
+
+        
+        
+        if not timestamp_match is None:
+            timestamp_groups = timestamp_match.groups()
+            
+            time1 = npt.nptime(int(timestamp_groups[0]),
+                         int(timestamp_groups[1]),
+                         int(timestamp_groups[2]),
+                         1000 * int(timestamp_groups[3]))  #goes straight to microseconds from seconds(?!)
+            time2 = npt.nptime(int(timestamp_groups[4]),
+                         int(timestamp_groups[5]),
+                         int(timestamp_groups[6]),
+                         1000 * int(timestamp_groups[7]))
+
+            if subtract:
+                td1 = time1 - offset_time  #subtraction returns a timedelta ARARRRRGHAAGwb WHY
+                td1_hours = int(td1.seconds) / 3600
+                td1_minutes = int(td1.seconds - 3600 * td1_hours) / 60
+                td1_seconds = int(td1.seconds - 3600 * td1_hours - 60 * td1_minutes)
+                time1 = time(td1_hours,
+                             td1_minutes,
+                             td1_seconds,
+                             td1.microseconds)
+
+                
+                td2 = time2 - offset_time
+                td2_hours = int(td2.seconds) / 3600
+                td2_minutes = int(td2.seconds - 3600 * td2_hours) / 60
+                td2_seconds = int(td2.seconds - 3600 * td2_hours - 60 * td2_minutes)
+                time2 = time(td2_hours,
+                             td2_minutes,
+                             td2_seconds,
+                             td2.microseconds)
+                    
+                
+            else:
+                time1 = time1 + offset_time  #but addition returns nptime
+                time2 = time2 + offset_time  
+            
+            # strftime milliseconds seem kind of fucked,
+            # so just use a format string
+            subout.write("%02d:%02d:%02d,%s --> %02d:%02d:%02d,%s" % (time1.hour,
+                                                                      time1.minute,
+                                                                      time1.second,
+                                                                      int(str(time1.microsecond)[:3]),
+                                                                      time2.hour,
+                                                                      time2.minute,
+                                                                      time2.second,
+                                                                      int(str(time2.microsecond)[:3])))
+            subout.write("\n")
+        else:
+            subout.write(line)
+            subout.write("\n")
+                    
+    subout.close()
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print 'usage: input_file.srt [-]hh:mm:ss,mss output_file.srt'
+        sys.exit()
+    main(*sys.argv[1:])
